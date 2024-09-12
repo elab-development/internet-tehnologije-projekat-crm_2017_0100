7bf7d077d9dc364b2cad82ac3335b64892c83812
Mateja Denic <mateja.denic2000@gmail.com>
2024-09-13 00:46:37 +0200

    Odradio sam promene poput razdvajanja funkcija koju su mi bile poput FetchLeadContact mi je sad fetchLead i fetchContact, dodavao sam neke dodatne varijabile za showAdd komponetne, sve zavisno da li se trazi lead ili contact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,14 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Contacts from './components/Contacts'
 import AddContactLeadComponent from './components/AddContactLeadComponent'
+import AddContact from './components/AddContact'
+import AddLead from './components/AddLead'
 import About from './components/About'
 import Leads from './components/Leads'
 
 const App = () => {
   const [showAddLead, setShowAddLead] = useState(false)
+  const [showAddContact, setShowAddContact] = useState(false)
   const [leads, setLeads] = useState([])
   const [contacts, setContacts] = useState([])
   const [page, setPage] = useState(1)
@@ -28,10 +31,9 @@ const App = () => {
     getLeads()
   }, [])
 
-  // Fetch Tasks
   const fetchLeads = async () => {
     try {
-      const res = await fetch('http://localhost:8000/leads');
+      const res = await fetch('http://localhost:8000/api/leads');
       if (!res.ok) {
         throw new Error('Failed to fetch leads');
       }
@@ -45,7 +47,7 @@ const App = () => {
   
   const fetchContacts = async () => {
     try {
-      const res = await fetch('http://localhost:9000/contacts');
+      const res = await fetch('http://localhost:8000/api/contacts');
       if (!res.ok) {
         throw new Error('Failed to fetch contacts');
       }
@@ -56,26 +58,28 @@ const App = () => {
       return [];
     }
   }
-  // Fetch Task
-  const fetchContactLead = async (lead, id) => {
-    if(lead.checkLead){
-      const res = await fetch(`http://localhost:8000/leads/${id}`)
+  
+  const fetchLead = async (lead, id) => {
+
+    const res = await fetch(`http://localhost:8000/api/leads/${id}`)
     const data = await res.json()
 
     return data
-    } else{
-      const res = await fetch(`http://localhost:9000/contacts/${id}`)
-    const data = await res.json()
-
-    return data
-    }
-    
   }
 
-  // Add Contact or Lead
-  const AddContactLead = async (lead) => {
-    if(lead.checkLead){
-      const res = await fetch('http://localhost:8000/leads', {
+  const fetchContact= async (lead, id) => {
+
+    const res = await fetch(`http://localhost:8000/api/contacts/${id}`)
+    const data = await res.json()
+
+    return data
+  }
+
+//Add Lead
+
+  const AddLead = async (lead) => {
+
+      const res = await fetch('http://localhost:8000/api/leads', {
         method: 'POST',
         headers: {
           'Content-type': 'application/json',
@@ -86,8 +90,17 @@ const App = () => {
       const data = await res.json()
   
       setLeads([...leads, data])
-    } else{
-      const res = await fetch('http://localhost:9000/contacts', {
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newLead = { id, ...lead }
+    // setLeads([...leads, newLead])
+  }
+
+//Add Contact
+
+  const AddContact = async (lead) => {
+    
+      const res = await fetch('http://localhost:8000/api/contacts', {
         method: 'POST',
         headers: {
           'Content-type': 'application/json',
@@ -98,43 +111,40 @@ const App = () => {
       const data = await res.json()
   
       setContacts([...contacts, data])
-    }
-    
-
-    // const id = Math.floor(Math.random() * 10000) + 1
-    // const newLead = { id, ...lead }
-    // setLeads([...leads, newLead])
   }
 
-  // Delete lead
-  const deleteContactLead = async (lead, id) => {
-    if(lead.checkLead){
-      const res = await fetch(`http://localhost:8000/leads/${id}`, {
+  // Delete Lead
+
+  const deleteLead = async (lead, id) => {
+      const res = await fetch(`http://localhost:8000/api/leads/${id}`, {
         method: 'DELETE',
       })
       //We should control the response status to decide if we will change the state or not.
       res.status === 200
         ? setLeads(leads.filter((lead) => lead.id !== id))
         : alert('Error Deleting This Lead')
-    } else{
-      const res = await fetch(`http://localhost:9000/contacts/${id}`, {
+  }
+
+  //Delete Contact
+
+  const deleteContact = async (lead, id) => {
+    
+      const res = await fetch(`http://localhost:8000/api/contacts/${id}`, {
         method: 'DELETE',
       })
       //We should control the response status to decide if we will change the state or not.
       res.status === 200
         ? setContacts(contacts.filter((contact) => contact.id !== id))
         : alert('Error Deleting This Lead')
-    }
-    
   }
 
   // Toggle 
   const toggleCheckLead = async (lead, id) => {
-    const objectToToggle = await fetchContactLead(lead, id)
+    const objectToToggle = await fetchLead(lead, id)
     if(lead.checkLead){
-      deleteContactLead(lead, id)
+      deleteLead(lead, id)
       const updObj = { ...objectToToggle, checkLead: !objectToToggle.checkLead }
-      AddContactLead(updObj)
+      AddLead(updObj)
     // const res = await fetch(`http://localhost:8000/leads/${id}`
     // , {
     //   method: 'PUT',
@@ -171,9 +181,9 @@ const App = () => {
     //   )
     // )
     } else{
-      deleteContactLead(lead, id)
+      deleteLead(lead, id)
       const updObj = { ...objectToToggle, checkLead: !objectToToggle.checkLead }
-      AddContactLead(updObj)
+      AddLead(updObj)
     }
     
   }
@@ -183,19 +193,22 @@ const App = () => {
     <Router>
       <div className='container'>
         <Header
-          onAdd={() => setShowAddLead(!showAddLead)}
-          showAdd={showAddLead}
+          onAddLead={() => setShowAddLead(!showAddLead)}
+          onAddContact={() => setShowAddLead(!showAddContact)}
+          showAddLead={showAddLead}
+          showAddContact={showAddContact}
         />
         <Route
           path='/'
           exact
           render={(props) => (
             <>
-              {showAddLead && <AddContactLeadComponent onAdd={AddContactLead} />}
-              {leads.length > 0 ? (
+              {showAddLead && <AddLead onAdd={AddLead} />}
+              {showAddContact && <AddContact onAdd={AddContact} />}
+              {contacts.length > 0 ? (
                 <Contacts
                   contacts={contacts}
-                  onDelete={deleteContactLead}
+                  onDelete={deleteContact}
                   onToggle={toggleCheckLead}
                   page={page}
                   setPage={setPage}
@@ -211,7 +224,7 @@ const App = () => {
           <>
             {leads.length > 0 ? (
             <Leads
-            onDelete={deleteContactLead}
+            onDelete={deleteLead}
             onToggle={toggleCheckLead}
               leads={leads}
               page={page}

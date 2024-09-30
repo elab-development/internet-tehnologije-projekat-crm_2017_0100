1d4c18f9e3791cfc443342709d23eae9f1f7d112
Mateja Denic <mateja.denic2000@gmail.com>
2024-09-30 19:01:39 +0200

    Dodavanje opcije search-a

--- frontend/src/components/Lead.js
+++ frontend/src/components/Lead.js
@@ -1,24 +1,25 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { useAuth } from './AuthProvider'
 
 const Lead = ({lead, onDelete, onToggle}) => {
-
+    const { user } = useAuth()
 return (
 <div>
     <div
             className={`lead ${lead.checkLead ? 'border-green' : ''}`}
             onDoubleClick={() => onToggle(lead, lead.id)}
         >
-            <h3>
+            <h3 className={user === null ? 'blurred' : ''}>
             {lead.name}{' '}
             <FaTimes
                 style={{ color: 'red', cursor: 'pointer' }}
                 onClick={() => onDelete(lead, lead.id)}
             />
             </h3>
-            <p>{lead.email}</p>
-            <p>{lead.phone}</p>
-            <p>{lead.company}</p>
+            <p className={user === null ? 'blurred' : ''}>{lead.email}</p>
+            <p className={user === null ? 'blurred' : ''}>{lead.phone}</p>
+            <p className={user === null ? 'blurred' : ''}>{lead.company}</p>
         </div>
 </div>
 )
--- frontend/src/components/Leads.js
+++ frontend/src/components/Leads.js
@@ -1,25 +1,37 @@
 import React from 'react'
 import Lead from './Lead'
+
 import { useAuth } from './AuthProvider';
 
-const Leads = ({ leads, onDelete, onToggle, page, setPage}) => {
+const Leads = ({ leads, onDelete, onToggle, page, setPage, onChange,searchText,searchCriteria, setSearchCriteria, setSearchText}) => {
     const itemsPerPage = 5;
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
     const currentLeads = leads.slice(startIndex, endIndex);
 
-    const { user } = useAuth();
+    const { user } = useAuth()
 
 return (
     <div>
         <h2>
             Leads 
         </h2>
-        <h1>{console.log(user)}</h1>
         {currentLeads.map((lead, index) => (
         <Lead key={index} lead={lead}  onDelete={onDelete} onToggle={onToggle}/>
         ))}
         <span>
+        <div className={user? '' : 'blurred'}>
+            <input type="text" className="search-lead" placeholder="Search..." onChange={(e) => {
+                setSearchText(e.target.value)                
+            }}/>
+            <span>Search criteria:</span>
+            <select name="select-criteria" id="select-criteria" onClick={(e) => setSearchCriteria(e.target.value)}>
+                <option value="name">First and Last Name</option>
+                <option value="email">Email</option>
+                <option value="phone">Phone Number</option>
+            </select>
+            <button className='btn' onClick={onChange}>Trazi</button>
+        </div>
         <button id='previous-page' className='btn-page' 
         onClick={() => setPage(page -1)} disabled={page === 1}>Previous Page</button> 
         <p><strong>Page {page}</strong></p>

065b888c85203d4f920fcdcf72c5e597ef22229a
Mateja Denic <mateja.denic2000@gmail.com>
2024-09-30 18:52:39 +0200

    Dodao sam web servis i podatke koje sam povuko sam prikazao u komponenti

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,8 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 const About = () => {
   const [nameForCrm, setNameForCrm]= useState("MyCRMApp");
+  const url = "https://reqres.in/api/users?page=2";
+  const [oldLeads, setOldLeads] = useState([])
+
+  useEffect(()=>{
+    axios.get(url)
+      .then(res => {
+        setOldLeads(res.data.data)
+      })
+      .catch(err=>console.log("Message is: "+ err));
+
+  }, [])
+
   return (
     <div>
       <h2>About Us</h2>
@@ -12,6 +25,10 @@ const About = () => {
       <p>
         Our mission is to provide you with the tools to easily track customer interactions, run marketing campaigns, and improve sales results. With <strong>{nameForCrm}</strong>, you can focus on growing your business and achieving success.
       </p>
+      <h3 className='margin-top'>Some of the past clients:</h3>
+      {oldLeads.map((lead) => (
+        <p key={lead.id}> Name: {lead.first_name} ; email: {lead.email}</p>
+      ))}
       <Link to='/' className='btn'>Go Back</Link>
     </div>
   )
